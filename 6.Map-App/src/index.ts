61bf58fedb403c1f265a273e070bef2ebc1938f0
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-04-17 06:52:36 -0500

    Adding map to html

--- 6.Map-App/src/index.ts
+++ 6.Map-App/src/index.ts
@@ -2,3 +2,10 @@
 import { User } from './User';
 import { Company } from './Compay';
 
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: {
+        lat:0,
+        lng:0
+    }
+});
