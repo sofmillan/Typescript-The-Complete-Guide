2c9824e8d689f7fabd22a0fd800f398cb5badaa1
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-04-18 07:51:38 -0500

    Refactoring markers

--- 6.Map-App/src/CustomMap.ts
+++ 6.Map-App/src/CustomMap.ts
@@ -1,5 +1,9 @@
-import { User } from "./User";
-import { Company } from "./Compay";
+interface Mappable{
+    location:{
+        lat:number;
+        lng:number;
+    }
+}
 
 export class CustomMap{
     private googleMap: google.maps.Map;
@@ -13,18 +17,15 @@ export class CustomMap{
             }
         });
     }
-
-    addUserMarker(user: User){
+    
+    addMarker(mappable: Mappable): void{
         new google.maps.Marker({
             map: this.googleMap,
             position:{
-                lat: user.location.lat,
-                lng: user.location.lng
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
             }
         })
     }
 
-    addCompanyMarker(company:Company){
-
-    }
 }
