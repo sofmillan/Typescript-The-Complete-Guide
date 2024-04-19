74c4139a42430cf0bf1b39180bb358de9ae691ad
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-04-19 09:13:41 -0500

    Sorting implementation for array of numbers

--- 7.Sort-App/src/index.ts
+++ 7.Sort-App/src/index.ts
@@ -3,6 +3,16 @@ class Sorter{
     }
 
     sort():void{
-        
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length-i-1; j++) {
+                if(this.collection[j]>this.collection[j+1]){
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j+1];
+                    this.collection[j+1] = temp;
+                }
+            }
+            
+        }
     }
 }
