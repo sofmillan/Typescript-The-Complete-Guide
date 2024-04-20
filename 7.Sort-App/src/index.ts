d607c7dab80557b393efdba36abbd7f55255b664
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-04-20 11:25:59 -0500

    Adding abstract class

--- 7.Sort-App/src/index.ts
+++ 7.Sort-App/src/index.ts
@@ -1,11 +1,22 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10,3,-2,50]);
+
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
 const charactersCollection = new CharactersCollection("ricky");
-
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(50);
+linkedList.add(10);
+linkedList.add(-9);
+linkedList.add(1000);
+
+linkedList.sort();
+console.log(linkedList.print());
