cf8ae7a67b1684bf5bc2ac6d00f805bd151fbb41
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-04-22 15:42:34 -0500

    Using enums

--- 8.Stats-App/build/index.js
+++ 8.Stats-App/build/index.js
@@ -1,2 +1,26 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+}).split('\n')
+    .map((row) => row.split(','));
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- 8.Stats-App/src/index.ts
+++ 8.Stats-App/src/index.ts
@@ -5,3 +5,20 @@ const matches = fs.readFileSync('football.csv',{
 }).split('\n')
 .map((row:string):string[] => row.split(','));
 
+enum MatchResult{
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
+
+let manUnitedWins = 0;
+
+for(let match of matches) {
+    if(match[1]=== 'Man United' && match[5] === MatchResult.HomeWin){
+        manUnitedWins++;
+    }else if(match[2]=== 'Man United' && match[5] === MatchResult.AwayWin){
+        manUnitedWins++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
