014760f551a98a37488e532c3c985cee308344ad
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-05-20 07:50:03 -0500

    Extracting an Attributes Class

--- 10.Web-Framework-App/src/models/User.ts
+++ 10.Web-Framework-App/src/models/User.ts
@@ -11,16 +11,5 @@ export class User{
 
     public events: Eventing = new Eventing();
     public sync: Sync<UserProps> = new Sync<UserProps>("http://localhost:3000/users");
-
-    constructor(private data: UserProps){}
-
-    get(propName: string): (string | number){
-        return this.data[propName];
-    }
-
-    set(update: UserProps):void{
-        Object.assign(this.data, update);
-    }
-
     
 }
