8d90e76c747a026fc1994d4bf18a3558887022fa
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-05-22 08:28:56 -0500

    Extending Model and Refactoring

--- 10.Web-Framework-App/src/models/User.ts
+++ 10.Web-Framework-App/src/models/User.ts
@@ -1,60 +1,24 @@
 import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import {Sync} from './Sync';
 import { Attributes } from './Attributes';
+import { Model } from './Model';
+import { ApiSync } from './ApiSync';
+
 export interface UserProps{
     id?:number;
     name?: string;
     age?:number;
 }
 
-export class User{
+const rootUrl = 'http://localhost:3000/users'
 
-    public events: Eventing = new Eventing();
-    public sync: Sync<UserProps> = new Sync<UserProps>("http://localhost:3000/users");
-    
-    public attributes: Attributes<UserProps>;
-
-    constructor(attrs: UserProps) {
-        this.attributes = new Attributes<UserProps>(attrs);
+export class User extends Model<UserProps>{
+    static buildUser(attrs: UserProps):User{
+        return new User(
+            new Attributes<UserProps>(attrs),
+            new Eventing(),
+            new ApiSync<UserProps>(rootUrl)
+        );
     }
-
-    get on(){
-        return this.events.on;
-    }
-
-    get trigger(){
-        return this.events.trigger;
-    }
-
-    get get(){
-        return this.attributes.get;
-    }
-
-    set(update:UserProps):void{
-        this.attributes.set(update);
-        this.events.trigger('change');
-    }
-    fetch(): void{
-        const id = this.attributes.get('id');
-
-        if(typeof id !== 'number'){
-            throw new Error('Cannot fetch without id');
-        }
-
-        this.sync.fetch(id).then((response:AxiosResponse) =>{
-        this.set(response.data);
-        })
-    }
-
-    save(): void{
-        this.sync.save(this.attributes.getAll())
-        .then((response:AxiosResponse) =>{
-            this.trigger('save');
-        })
-        .catch(()=>{
-            this.trigger('error');
-        });
-
-    }
+   
 }
