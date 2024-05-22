f8cb3f45ded4a68219634c06bf22bf58b6287be9
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-05-22 10:55:02 -0500

    Generic User Collection

--- 10.Web-Framework-App/src/index.ts
+++ 10.Web-Framework-App/src/index.ts
@@ -1,2 +1,13 @@
-import { User } from "./models/User";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
+const collection = new Collection<User, UserProps>(
+    'http://localhost:3000/users',
+    (json:UserProps) => User.buildUser(json)
+);
+
+collection.on('change', ()=>{
+    console.log(collection);
+})
+
+collection.fetch();
