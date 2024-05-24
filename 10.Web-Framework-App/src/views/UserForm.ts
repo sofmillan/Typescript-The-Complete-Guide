ebf4fd0fef1d84222bdb0a3d51e35bca3e242f96
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-05-23 20:08:23 -0500

    Adding Model Properties

--- 10.Web-Framework-App/src/views/UserForm.ts
+++ 10.Web-Framework-App/src/views/UserForm.ts
@@ -1,17 +1,44 @@
+import {User} from '../models/User'
+
 export class UserForm{
-    constructor(public parent: Element){}
+    constructor(public parent: Element, public model:User){}
+
+    eventsMap():{[key: string]: () =>void}{
+        return {
+            'click:button':this.onButtonClick,
+            'mouseover:h1':this.onHeaderHover
+        }
+    }
+    onHeaderHover():void{console.log("H1");}
+    onButtonClick():void{console.log("Click");}
 
     template():string{
         return `
         <div>
             <h1>User Form</h1>
+            <button>Click me</button>
         </div>
         `
+    }
+
+    bindEvents(fragment: DocumentFragment):void{
+        const eventsMap = this.eventsMap();
+
+        for(let eventKey in eventsMap){
+            const [eventName, selector] = eventKey.split(':');
+
+            fragment.querySelectorAll(selector).forEach(element =>{
+                element.addEventListener(eventName, eventsMap[eventKey])
+            })
+        }
     }
 
     render():void{
         const templateElement = document.createElement('template');
         templateElement.innerHTML = this.template();
+
+        this.bindEvents(templateElement.content);
+
         this.parent.append(templateElement.content); 
     }
 }
