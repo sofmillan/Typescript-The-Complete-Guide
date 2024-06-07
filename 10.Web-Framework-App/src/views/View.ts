0b3fbd32fae57dbf98c3f4b8307e5aa0ce249565
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-07 15:09:38 -0500

    View Nesting

--- 10.Web-Framework-App/src/views/View.ts
+++ 10.Web-Framework-App/src/views/View.ts
@@ -43,6 +43,7 @@ export abstract class View<T extends Model<K>, K extends HasId> {
             }
         }
     }
+    onRender():void{}
 
     render():void{
         this.parent.innerHTML = '';
@@ -51,6 +52,7 @@ export abstract class View<T extends Model<K>, K extends HasId> {
 
         this.bindEvents(templateElement.content);
         this.mapRegions(templateElement.content);
+        this.onRender();
         this.parent.append(templateElement.content); 
     }
 }
