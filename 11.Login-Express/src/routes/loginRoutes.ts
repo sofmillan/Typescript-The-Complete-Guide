51224876e0a94ddbce90a83b1f9bd9c1d6fac224
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-08 10:41:47 -0500

    Parsing body

--- 11.Login-Express/src/routes/loginRoutes.ts
+++ 11.Login-Express/src/routes/loginRoutes.ts
@@ -3,10 +3,25 @@ import {Request, Response} from 'express';
 
 const router = Router();
 
-router.get('/', (req:Request, res:Response)=>{
+router.get('/login', (req:Request, res:Response)=>{
     res.send(`
-        <div>
-        <h1>Hi!</h1></div>`)
+        <form method="POST">
+            <div>
+                <label>Email</label>
+                <input name="email">
+            </div>
+             <div>
+                <label>Password</label>
+                <input name="password" type="password">
+            </div>
+            <button>Submit</button>
+        </form>
+        `)
+})
+
+router.post('/login', (req:Request, res:Response)=>{
+    const {email, password} = req.body;
+    res.send(email)
 })
 
 export {router};
