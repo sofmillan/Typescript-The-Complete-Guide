bc5b2151394dba348262492f7e81a5cdbce3b506
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-06-10 20:12:14 -0500

    Logging out

--- 11.Login-Express/src/routes/loginRoutes.ts
+++ 11.Login-Express/src/routes/loginRoutes.ts
@@ -41,17 +41,21 @@ router.get('/', (req:Request, res:Response)=>{
         res.send(`
             <div>
             <div>You are logged in</div>
-            <a href="/logout"></a>
+            <a href="/logout">Log out</a>
             </div>
             `)
     }else{
         res.send(`
             <div>
             <div>You are not logged in</div>
-            <a href="/login"></a>
+            <a href="/login">Log in</a>
             </div>
             `)
     }
 })
 
+router.get('/logout', (req:Request, res:Response)=>{
+    req.session = undefined;
+    res.redirect('/');
+})
 export {router};
